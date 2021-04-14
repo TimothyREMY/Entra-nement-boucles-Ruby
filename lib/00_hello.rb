79b97da7fe7d2b245ea61a0ba0e2d4022cae23d5
TimothyREMY <timothy.remy@gmail.com>
2021-04-14 15:50:37 +0200

    Il a pas dit bonjour

---
+++ lib/00_hello.rb
@@ -0,0 +1,18 @@
+puts "Il faut que tu dises bonjour !"
+print ">"
+input = gets.chomp
+
+while input != "Bonjour"
+    puts "Il a pas dit bonjour !"
+    puts "On va niquer ta mère !"
+    puts "Bonjour madame, on va niquer ta mère"
+    puts "Il a niqué la mère de sa mère pour enfin niqué sa mère"
+    puts "Naaaaan valait mieux dire bonjour"
+    puts "Mais lui il a pas dit bonjour"
+    puts "Il s'est fait niquer sa mère"
+    puts " "
+    puts "Est-ce qu'il va dire bonjour ?"
+    input = gets.chomp
+end
+
+puts "Une parenthèse en charentaise, il a dit bonjour"
